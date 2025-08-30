afa72145ceb1fcfa58fa15ebc1419de929f6cf21
Ylfmoonn <lapu2023@outlook.com>
2025-08-30 22:21:02 +0800

    first init

---
+++ src/main/java/org/csu/pixelstrikebackend/PixelStrikeBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.csu.pixelstrikebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelStrikeBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixelStrikeBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/pixelstrikebackend/PixelStrikeBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.pixelstrikebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelStrikeBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
