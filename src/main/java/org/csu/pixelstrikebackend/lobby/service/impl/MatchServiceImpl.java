1a494511c33bdacb9a1b462a0643707d29ae1db0
徐梓程 <2779155474@qq.com>
2025-09-13 11:39:04 +0800

    修复了前端游戏结束后玩家的状态问题

--- src/main/java/org/csu/pixelstrikebackend/lobby/service/impl/MatchServiceImpl.java
+++ src/main/java/org/csu/pixelstrikebackend/lobby/service/impl/MatchServiceImpl.java
@@ -3,10 +3,13 @@ package org.csu.pixelstrikebackend.lobby.service.impl;
 import org.csu.pixelstrikebackend.lobby.entity.Match;
 import org.csu.pixelstrikebackend.lobby.entity.MatchParticipant;
 import org.csu.pixelstrikebackend.lobby.entity.UserProfile;
+import org.csu.pixelstrikebackend.lobby.enums.UserStatus;
+import org.csu.pixelstrikebackend.lobby.mapper.FriendMapper;
 import org.csu.pixelstrikebackend.lobby.mapper.MatchMapper;
 import org.csu.pixelstrikebackend.lobby.mapper.MatchParticipantMapper;
 import org.csu.pixelstrikebackend.lobby.mapper.UserProfileMapper;
 import org.csu.pixelstrikebackend.lobby.service.MatchService;
+import org.csu.pixelstrikebackend.lobby.service.OnlineUserService;
 import org.csu.pixelstrikebackend.lobby.websocket.WebSocketSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,7 +26,8 @@ public class MatchServiceImpl implements MatchService {
     @Autowired private MatchParticipantMapper matchParticipantMapper;
     @Autowired private UserProfileMapper userProfileMapper;
     @Autowired private WebSocketSessionManager webSocketSessionManager;
-
+    @Autowired private FriendMapper friendMapper;
+    @Autowired private OnlineUserService onlineUserService;
     /**
      * 此方法现在只负责持久化战绩和更新用户资料，不再管理在线状态。
      */
@@ -57,7 +61,39 @@ public class MatchServiceImpl implements MatchService {
                 "results", results
         );
         for (MatchParticipant participant : results) {
+            onlineUserService.updateUserStatus(participant.getUserId(), UserStatus.ONLINE);
+            notifyFriendsAboutStatusChange(participant.getUserId(), "ONLINE");
             webSocketSessionManager.sendMessageToUser(participant.getUserId(), resultMessage);
         }
     }
+
+    /**
+     * 辅助方法，用于通知好友状态变更
+     * @param userId 状态变更的用户ID
+     * @param status 新的状态 (例如 "IN_GAME")
+     */
+    private void notifyFriendsAboutStatusChange(Integer userId, String status) {
+        List<UserProfile> friends = friendMapper.selectFriendsProfiles(userId);
+        if (friends.isEmpty()) {
+            return;
+        }
+
+        UserProfile userProfile = userProfileMapper.selectById(userId);
+        if (userProfile == null) return;
+
+        Map<String, Object> notification = Map.of(
+                "type", "status_update",
+                "userId", userId,
+                "nickname", userProfile.getNickname(), // **新增 nickname 字段**
+                "status", status
+        );
+
+        System.out.println("Notifying online friends of user " + userId + " about status change to " + status);
+        for (UserProfile friend : friends) {
+            if (onlineUserService.isUserOnline(friend.getUserId())) {
+                webSocketSessionManager.sendMessageToUser(friend.getUserId(), notification);
+            }
+        }
+    }
+
 }
